d616adef86b76a18ed7f609f9b9caa41f9961d30
bredele <olivier.wietrich@gmail.com>
2013-10-24 18:55:10 -0600

    add blog and home components

---
+++ client/router/index.js
@@ -0,0 +1,15 @@
+
+/**
+ * Dependencies
+ */
+
+var page = require('page');
+
+
+//define routes
+
+page('/', require('home'));
+page('/blog/:post', require('blog'));
+//page('/blog/:repo/:post', edit);
+page('*', notfound);
+page();
